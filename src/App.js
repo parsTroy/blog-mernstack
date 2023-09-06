804299fe933f3fade94d6f8b0616b0c3f33bb19f
Troy <troyparsons@hotmail.com>
2023-09-06 16:24:07 -0400

    2: Broken due to UserContext js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import Header from './component/Header';
 import Layout from './component/Layout';
 import {Route, Routes} from 'react-router-dom';
 import Index from './pages/Index';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { UserContextProvider } from './context/UserContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Index />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Index />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,20 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 
 const Header = () => {
-  
-  useEffect(() => {
-    fetch('http://localhost:4000/profile', {
+
+  const {userInfo, setUserInfo} = useContext(UserContext);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:4000/profile', {
+  //     credentials: 'include',
+  //   }).then(response => {
+  //     response.json().then(userInfo => {
+  //       setUserInfo(userInfo);
+  //     })
+  //   });
+  // }, []);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
       credentials: 'include',
+      method: 'POST',
     });
-  }, []);
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
   
   return (
     <header>
-        <Link to="/" className="logo">My Blog</Link>
+        <Link to='/' className='logo'>My Blog</Link>
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {username && (
+            <>
+              <Link to='/create'>Post</Link>
+              <a href="/test" onClick={logout}>Logout</a>
+            </>
+          )}
+          {!username && (
+            <>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+            </>
+          )}
         </nav>
     </header>
   )
